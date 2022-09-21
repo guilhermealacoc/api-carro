d189dc802ab2c876275c25ce37ca61d0cb54d6e5
Guilherme de Alacoc Aquino <guilhermealacoc@gmail.com>
2022-09-21 00:02:34 -0300

    aguardando correcao

--- src/services/CarroService.js
+++ src/services/CarroService.js
@@ -3,7 +3,13 @@ const db = require("../db");
 module.exports = {
   buscarTodos: () => {
     return new Promise((aceito, rejeitado) => {
-      db.query("SELECT * FROM carros", (error, results));
+      db.query("SELECT * FROM carros", (error, results) => {
+        if (error) {
+          rejeitado(error);
+          return;
+        }
+        aceito(results);
+      });
     });
   },
 };
