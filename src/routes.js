10d7c1c92c3f76ee3984d4463c693e3c9ad6703f
Guilherme de Alacoc Aquino <guilhermealacoc@gmail.com>
2022-09-20 23:33:26 -0300

    inicio do projeto com arquivos

---
+++ src/routes.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const CarroController = require("./controllers/CarroController");
+
+router.get("/carros", CarroController.buscarTodos);
+
+module.exports = router;
